3a765553691cba1073edf240265fbdc7655b5d4b
victornoel <vnoel890@gmail.com>
2023-06-17 00:55:51 -0400

    More updates

--- giphy-search/src/components/seachfield.jsx
+++ giphy-search/src/components/seachfield.jsx
@@ -10,17 +10,17 @@ const SearchField = () => {
             const gifData = await axios.get("https://api.giphy.com/v1/gifs/search?q=" + search + "&api_key=ATQhfET4go8lHY0DIzHYlQpeRtEYSttj");
             // console.log(gifData.data.data[0].url);
             // const loneGifData = gifData.data.data[0].url;
-            setGifList(gifData.data.data.url);
-            console.log(gifList);
+            const arr = gifData.data.data;
+            console.log(arr);
+            arr.length -= 45;
+            setGifList(arr);
+
         } catch (error){
             console.error(error);
         }
+        
     }
-
-    
 // }, [])
-    
-    
     const handleSubmit = (event) => {
         event.preventDefault();
         const search = event.target[0].value;
@@ -37,14 +37,26 @@ const SearchField = () => {
                 <button type = "submit">Search</button>
             </form>
             <div>
-                {/* {gifList.map((gif) => {
-                    return <p key={gif.url}></p>}
-                )}; */}
-            </div>
-            
-        </div>
+            <img src="https://giphy.com/gifs/g1ft3d-art-glitch-apple-bfrlODgSLqXxS" alt="gif"></img>
 
+                {/* {gifList.map((gif) => {
+                    return (
+                        
+                        
+                        // <div key= {gif.id} >
+                        // <img src = {gif.url} alt="gif"></img>
+                        // </div>
+                    );
+                    
+                    }
+                )} */}
+            </div> 
+        </div>
     );
+            
+            
+            
 }
+
 // do something on submit of form
 export default SearchField;
