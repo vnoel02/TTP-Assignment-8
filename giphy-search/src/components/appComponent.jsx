0cdd8099f8259aa1860366b849dfec0fbebd937c
victornoel <vnoel890@gmail.com>
2023-06-18 01:07:14 -0400

    able to make search and trending work/reorganized

--- giphy-search/src/components/appComponent.jsx
+++ giphy-search/src/components/appComponent.jsx
@@ -1,14 +1,20 @@
 //just contain trending gifs on load as default
 import SearchField from "./seachfield";
 import GifCard from "./gifCard";
+import {fetchGif} from "./seachfield";
 import React, {useState, useEffect} from 'react';
 import axios from "axios";
-const AppComponent = () => {
+const AppComponent = (props) => {
     const [trending, setTrending] = useState([]);
+    const [search, setSearch] = useState([]);
+    // const [term, setTerm] = useState("");
+
+
+    // Fetchs and 'loads' trending images
     useEffect(() => {
         async function fetchTrending() {
             try {
-            const list = await axios.get("http://api.giphy.com/v1/gifs/trending?api_key=ATQhfET4go8lHY0DIzHYlQpeRtEYSttj&limit=5");
+            const list = await axios.get("http://api.giphy.com/v1/gifs/trending?api_key=ATQhfET4go8lHY0DIzHYlQpeRtEYSttj&limit=10");
             setTrending(list.data.data);
             console.log(list);
             } catch (error) {
@@ -18,31 +24,51 @@ const AppComponent = () => {
         fetchTrending();
     }, []);
 
+    // Fetches gifs based on search, changes state of trending gifs so they are no longer displayed
+    async function fetchSearch(search) {
+        try {
+            const gifData = await axios.get("https://api.giphy.com/v1/gifs/search?q=" + search + "&api_key=ATQhfET4go8lHY0DIzHYlQpeRtEYSttj&limit=10");
+            const arr = gifData.data.data;
+            console.log(arr);
+            setSearch(arr);
+            setTrending([]);
+        } catch (error){
+            console.error(error);
+        } 
+    }
+ 
+    // On click of the submit button, takes input to use in giphy API
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const search = event.target[0].value; //updates search state
+        fetchSearch(search);
+        console.log(search);
+    }
     
-
-
-    
-    return (  
+    return ( 
         <div>
             <div>
-                <SearchField /> 
+                {/* <SearchField  onSubmit={handleSubmit}/> */}
+                <h1>Giphy Search</h1>
+                <form onSubmit={handleSubmit}> 
+                    <label>Search</label>
+                    <input type="text"></input>
+                    <button type = "submit">Search</button>
+                </form>
+                <div>
+                    
+                </div> 
+                
             </div>
             <div>
-                {trending.map((trending) => {
-                    return (
-                        <div key= {trending.id} >
-                        <img id="display" src = {trending.images.fixed_height.url} alt="gif" autoPlay loop inline="true"></img>
-                        </div>
-                    );
-                    }
-                )}
+                {/* if clicked display search, if not display trending */}
+                {/* {isClicked? (<GifCard data={search}/>):(<GifCard data={trending} />)} */}
+                <GifCard data={search} />
+                <GifCard data={trending} />
+            
             </div> 
         </div>
     );
-
-
-   // Idea: try to set a prop to put into searchField, output gifs of 
-
 }
 
 export default AppComponent;
--- giphy-search/src/components/seachfield.jsx
+++ giphy-search/src/components/seachfield.jsx
@@ -1,52 +1,37 @@
 //Search Field
 import React, {useState} from 'react';
-import appComponent from './appComponent';
+import AppComponent from './appComponent';
 import axios from 'axios';
+import GifCard from './gifCard';
 
 const SearchField = (props) => {
-    const [gifList, setGifList] = useState([]);
-    async function fetchGif(search) {
-        try {
-            const gifData = await axios.get("https://api.giphy.com/v1/gifs/search?q=" + search + "&api_key=ATQhfET4go8lHY0DIzHYlQpeRtEYSttj&limit=5");
-            const arr = gifData.data.data;
-            console.log(arr);
-            setGifList(arr);
-            console.log(arr);
-        } catch (error){
-            console.error(error);
-        } 
-    }
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        const search = event.target[0].value;
-        fetchGif(search);
-        // console.log(search);
-    }
+    // const [gifList, setGifList] = useState([]);
+    // async function fetchGif(search) {
+    //     try {
+    //         const gifData = await axios.get("https://api.giphy.com/v1/gifs/search?q=" + search + "&api_key=ATQhfET4go8lHY0DIzHYlQpeRtEYSttj&limit=5");
+    //         const arr = gifData.data.data;
+    //         console.log(arr);
+    //         setGifList(arr);
+    //         console.log(arr);
+    //     } catch (error){
+    //         console.error(error);
+    //     } 
+    // }
+    // const handleSubmit = (event) => {
+    //     event.preventDefault();
+    //     const search = event.target[0].value;
+    //     fetchGif(search);
+    //     // console.log(search);
+    // }
     return (  
         <div>
-            <h1>Giphy Search</h1>
-            <form onSubmit={handleSubmit}> 
-                <label>Search</label>
+               <h1> Giphy Search</h1>
+            <form> 
                 <input type="text"></input>
                 <button type = "submit">Search</button>
             </form>
-            <div>
-                {gifList.map((gif) => {
-                    return (
-                        <div key= {gif.id} >
-                        
-                        <img id="display" src = {gif.images.fixed_height.url} alt="gif" autoPlay loop inline="true"></img>
-                        </div>
-                    );
-                    
-                    }
-                )}
-            </div> 
         </div>
-    );
-            
-            
-            
+    );         
 }
 
 // do something on submit of form
